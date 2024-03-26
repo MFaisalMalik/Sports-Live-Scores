691fe8e27ae10416167a736890dfd39e96b21d1a
Faisal Malik <faisal@retrocausal.ai>
2024-03-27 02:18:39 +0500

    Added sports card images

--- frontend/src/components/Cards.js
+++ frontend/src/components/Cards.js
@@ -10,36 +10,26 @@ function Cards() {
         <div className='cards__wrapper'>
           <ul className='cards__items'>
             <CardItem
-              src='images/img-9.jpg'
-              text='Explore the hidden waterfall deep inside the Amazon Jungle'
-              label='Adventure'
+              src='images/Football.jpg'
+              text='Football'
               path='/services'
             />
             <CardItem
-              src='images/img-2.jpg'
-              text='Travel through the Islands of Bali in a Private Cruise'
-              label='Luxury'
+              src='images/Baseball.jpg'
+              text='Baseball'            
               path='/services'
             />
           </ul>
           <ul className='cards__items'>
             <CardItem
-              src='images/img-3.jpg'
-              text='Set Sail in the Atlantic Ocean visiting Uncharted Waters'
-              label='Mystery'
+              src='images/Basketball.jpg'
+              text='Basketball'             
               path='/services'
             />
             <CardItem
-              src='images/img-4.jpg'
-              text='Experience Football on Top of the Himilayan Mountains'
-              label='Adventure'
+              src='images/Hockey.jpg'
+              text='Hockey'
               path='/products'
-            />
-            <CardItem
-              src='images/img-8.jpg'
-              text='Ride through the Sahara Desert on a guided camel tour'
-              label='Adrenaline'
-              path='/sign-up'
             />
           </ul>
         </div>
