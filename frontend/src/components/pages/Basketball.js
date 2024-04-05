e82937a4006b6eb4bd2a99cc0175bbc4a1226a98
daniyalmushtaq5 <daniyal.musht@gmail.com>
2024-04-05 16:48:36 +0500

    Added multiple bets features on all game pages

--- frontend/src/components/pages/Basketball.js
+++ frontend/src/components/pages/Basketball.js
@@ -1,6 +1,78 @@
 import React from 'react';
 import '../../App.css';
+import { Button } from "../Button";
+import Footer from "../Footer";
 
 export default function Basketball() {
-  return <h1 className='basketball'>BASKETBALL</h1>;
+  return (
+    <>
+      <h1 className="basketball">BASKETBALL</h1>
+      <div className="container">
+        <div className="categories">
+          <div className="row">
+            <div className="col-3">
+              <div className="game-bets-free">
+                <h2 className="text-class-2">Free Game Bets</h2>
+                <Button
+                  className="btns"
+                  buttonStyle="btn--primary"
+                  buttonSize="btn--large"
+                  to="/sign-in"
+                  name="search"
+                >
+                  SEARCH
+                </Button>
+              </div>
+            </div>
+
+            <div className="col-3">
+              <div className="players-bets-free">
+                <h2 className="text-class-2">Free Players Bets</h2>
+                <Button
+                  className="btns"
+                  buttonStyle="btn--primary"
+                  buttonSize="btn--large"
+                  to="/sign-in"
+                  name="search-player-free"
+                >
+                  SEARCH
+                </Button>
+              </div>
+            </div>
+
+            <div className="col-3">
+              <div className="game-bets-premium">
+                <h2 className="text-class">Premium Game Bets</h2>
+                <Button
+                  className="btns"
+                  buttonStyle="btn--primary"
+                  buttonSize="btn--large"
+                  to="/sign-in"
+                  name="search-premium"
+                >
+                  SEARCH
+                </Button>
+              </div>
+            </div>
+
+            <div className="col-3">
+              <div className="palyer-bets-premium">
+                <h2 className="text-class">Premium Players Bets</h2>
+                <Button
+                  className="btns"
+                  buttonStyle="btn--primary"
+                  buttonSize="btn--large"
+                  to="/sign-in"
+                  name="search-premium"
+                >
+                  SEARCH
+                </Button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
 }
--- frontend/src/components/pages/Football.js
+++ frontend/src/components/pages/Football.js
@@ -1,27 +1,78 @@
-import React from 'react';
-import '../../App.css';
-import { Button } from '../Button';
-import Footer from '../Footer';
+import React from "react";
+import "../../App.css";
+import { Button } from "../Button";
+import Footer from "../Footer";
 
 export default function Football() {
-  return ( 
+  return (
     <>
-        <h1 className='football'>FOOTBALL</h1>
-        <div className='container'>
-            <div className='game-container'>
-                <h2 className='game-bets'>Top Game Bets</h2>
+      <h1 className="football">FOOTBALL</h1>
+      <div className="container">
+        <div className="categories">
+          <div className="row">
+            <div className="col-3">
+              <div className="game-bets-free">
+                <h2 className="text-class-2">Free Game Bets</h2>
                 <Button
-                className='btns'
-                buttonStyle='btn--primary'
-                buttonSize='btn--large'
-                to='/sign-in'
-                name='search'
+                  className="btns"
+                  buttonStyle="btn--primary"
+                  buttonSize="btn--large"
+                  to="/sign-in"
+                  name="search"
                 >
-                SEARCH
+                  SEARCH
                 </Button>
+              </div>
             </div>
+
+            <div className="col-3">
+              <div className="players-bets-free">
+                <h2 className="text-class-2">Free Players Bets</h2>
+                <Button
+                  className="btns"
+                  buttonStyle="btn--primary"
+                  buttonSize="btn--large"
+                  to="/sign-in"
+                  name="search-player-free"
+                >
+                  SEARCH
+                </Button>
+              </div>
+            </div>
+
+            <div className="col-3">
+              <div className="game-bets-premium">
+                <h2 className="text-class">Premium Game Bets</h2>
+                <Button
+                  className="btns"
+                  buttonStyle="btn--primary"
+                  buttonSize="btn--large"
+                  to="/sign-in"
+                  name="search-premium"
+                >
+                  SEARCH
+                </Button>
+              </div>
+            </div>
+
+            <div className="col-3">
+              <div className="palyer-bets-premium">
+                <h2 className="text-class">Premium Players Bets</h2>
+                <Button
+                  className="btns"
+                  buttonStyle="btn--primary"
+                  buttonSize="btn--large"
+                  to="/sign-in"
+                  name="search-premium"
+                >
+                  SEARCH
+                </Button>
+              </div>
+            </div>
+          </div>
         </div>
-        <Footer/>
+      </div>
+      <Footer />
     </>
-  )
+  );
 }
