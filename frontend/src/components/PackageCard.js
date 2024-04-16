1101a76a9dc670f72214e976a71aa4ca1abcb502
Faisal Malik <faisal@retrocausal.ai>
2024-04-16 16:02:31 +0500

    Added a component for a package card

---
+++ frontend/src/components/PackageCard.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Button } from './Button';
+
+function PackageCard(props) {
+  return (
+    <>
+      <li className="cards__item">
+        <div className="cards__item__link package_item">
+            <p className="package__name">{props.name}</p>
+            <p className="package__price">{props.price}</p>
+            <Button name= 'select' buttonStyle='btn--rounded' buttonSize='btn--large' to='/sign-up'>Select</Button>            
+        </div>
+      </li>
+    </>
+  );
+}
+
+export default PackageCard;
