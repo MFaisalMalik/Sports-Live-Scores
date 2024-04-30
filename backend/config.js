eb9ea3c9287a50e46bce5de4ebb5506db3263e83
Faisal Malik <faisal@retrocausal.ai>
2024-04-30 21:56:30 +0500

    Connected firebase with nodejs

--- backend/config.js
+++ backend/config.js
@@ -1,12 +1,33 @@
-import { initializeApp} from 'firebase/app'
+import dotenv from 'dotenv';
+import assert from 'assert';
 
-const firebaseConfig = {
-    apiKey: "AIzaSyAxd2rsady7HXyv5QHCTFsJdhYA7o_zmBs",
-    authDomain: "simplicity-sport-bets.firebaseapp.com",
-    projectId: "simplicity-sport-bets",
-    storageBucket: "simplicity-sport-bets.appspot.com",
-    messagingSenderId: "575596553356",
-    appId: "1:575596553356:web:ac70a74ef04ecedeb347a9"
+dotenv.config();
+
+const {
+  PORT,
+  HOST,
+  HOST_URL,
+  API_KEY,
+  AUTH_DOMAIN,
+  PROJECT_ID,
+  STORAGE_BUCKET,
+  MESSAGING_SENDER_ID,
+  APP_ID,
+} = process.env;
+
+assert(PORT, 'Port is required');
+assert(HOST, 'Host is required');
+
+export default {
+  port: PORT,
+  host: HOST,
+  hostUrl: HOST_URL,
+  firebaseConfig: {
+    apiKey: API_KEY,
+    authDomain: AUTH_DOMAIN,
+    projectId: PROJECT_ID,
+    storageBucket: STORAGE_BUCKET,
+    messagingSenderId: MESSAGING_SENDER_ID,
+    appId: APP_ID,
+  },
 };
-
-initializeApp(firebaseConfig);
