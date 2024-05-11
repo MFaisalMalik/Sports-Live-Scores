325862b8ce04baf92c1b5e66a35cbf40ebbf6608
Faisal Malik <faisal@retrocausal.ai>
2024-05-12 01:59:34 +0500

    Added authentication function

---
+++ frontend/src/firebase/auth.js
@@ -0,0 +1,36 @@
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+import { auth } from './firebase';
+
+export const doCreateUserWithEmailAndPassword = async (email, password) => {
+    return createUserWithEmailAndPassword(auth, email, password);
+}
+
+export const doSignInWithEmailAndPassword = async (email, password) => {
+    return signInWithEmailAndPassword(auth, email, password);
+}
+
+export const doSignInWithGoogle = async (email, password) => {
+    const provider = new GoogleAuthProvider();
+    const result = await signInWithPopup(auth, provider);
+
+    // result.user store this in filestore
+    return result;
+}
+
+export const doSignOut = () => {
+    return auth.signOut();
+}
+
+// export const doPasswordReset = (email) => {
+//     return sendPasswordResetEmail(auth, email);
+// };
+
+// export const doPasswordChange = (password) => {
+//     return updatePassword(auth.currentUser, password);
+// };
+
+// export const doSendEmailVerification = () => {
+//     return sendEmailVerification(auth.currentUser, {
+//         url: `${window.location.origin}/home`,
+//     });
+// };
