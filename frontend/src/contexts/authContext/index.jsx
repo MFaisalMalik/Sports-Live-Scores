6081ad6369366946d7b7251da8dd5a44cda8dbbd
Faisal Malik <faisal@retrocausal.ai>
2024-05-12 02:00:23 +0500

    created auth hook

---
+++ frontend/src/contexts/authContext/index.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import {useState, useEffect, useContext} from 'react';
+import { onAuthStateChanged } from 'firebase/auth';
+import { auth } from '../../firebase/firebase';
+
+const AuthContext = React.createContext();
+
+export function useAuth() {
+    return useContext(AuthContext);
+}
+
+export function AuthProvider ( {children} ) {
+    const [currentUser, setCurrentUser] = useState(null);
+    const [userLoggedIn, setUserLoggedIn] = useState(false);
+    const [loading, setLoading] = useState(true); // code is trying to load what is the current state of the user
+
+    useEffect (() => {
+        const unsubscribe = onAuthStateChanged(auth, initializeUser);
+        return unsubscribe;
+    }, [])
+
+    async function initializeUser(user) {
+        if (user) {
+            setCurrentUser({ ...user });
+            setUserLoggedIn(true);
+        } else {
+            setCurrentUser(null);
+            setUserLoggedIn(false);
+        }
+        setLoading(false);
+    }
+
+    const value = {
+        currentUser,
+        userLoggedIn,
+        loading
+    }
+
+    return (
+        <AuthContext.Provider value={value}>
+            {!loading && children}
+        </AuthContext.Provider>
+    );
+
+}
