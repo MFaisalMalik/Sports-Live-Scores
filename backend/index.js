7c9e3e5da00f26dfc4e33b57f4fbaa50c13d3c3b
AbdulrehmanTs <rohi644@gmail.com>
2024-06-28 00:00:34 +0500

    removed doubles

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,6 @@ import userRoutes from './routes/userRoute.js';
 import gameRoutes from './routes/gamesRoute.js';
 import blogRoutes from './routes/blogs.js';
 import subscriptionRoutes from './routes/subscriptionRoute.js';
-import subscriptionRoutes from './routes/subscriptionRoute.js';
 
 const app = express();
 
@@ -17,7 +16,6 @@ app.use(express.json());
 app.use('/api/user', userRoutes);
 app.use('/api/games', gameRoutes);
 app.use('/api/blogs', blogRoutes);
-app.use('/api/subscription', subscriptionRoutes);
 app.use('/api/subscription', subscriptionRoutes);
 
 app.listen(config.port, () =>
